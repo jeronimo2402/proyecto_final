4ba706dd733319b2c1907df05645ac0d5e083e14
jmn <jeronimomurillonarvaez@gmail.com>
2023-11-12 23:33:45 -0500

    Archivo index.php limpio. Carpeta llamada Includes donde se almacen las partes html

---
+++ includes/form.php
@@ -0,0 +1,21 @@
+<form class="row g-3">
+    <div class="col-auto">
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Departamento</option>
+            <option value="1">Caldas</option>
+            <option value="2">Barranquilla</option>
+            <option value="3">Chile</option>
+        </select>
+    </div>
+    <div class="col-auto">
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Ciudad</option>
+            <option value="1">Manizales</option>
+            <option value="2">Bogota</option>
+            <option value="3">Risaralda</option>
+        </select>
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+    </div>
+</form>
